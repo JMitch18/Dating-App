4cc984faf8493f9aa9d38b7796347363326e8f6f
Jaron <ronmitchell1@yahoo.com>
2021-12-10 12:40:30 -0600

    Imlemented photo upload functionality

--- src/app/_modules/shared.module.ts
+++ src/app/_modules/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs'
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 
@@ -10,16 +12,21 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
   declarations: [],
   imports: [
     CommonModule,
+    BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
     TabsModule.forRoot(),
-    NgxGalleryModule
+    NgxGalleryModule,
+    FileUploadModule, 
   ],
   exports: [
+    BsDropdownModule,
     ToastrModule,
     TabsModule,
-    NgxGalleryModule 
+    NgxGalleryModule,
+    FileUploadModule
+    
   ]
 })
 export class SharedModule { }
